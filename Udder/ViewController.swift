86238f798cbe93ce09d602f4367407bfc661e979
Char Kai <charkai@gmail.com>
2020-02-13 23:46:21 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Char Shu Kai on 13/2/2020.
+//  Copyright © 2020 Char Shu Kai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
